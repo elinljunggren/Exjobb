26753c5234cecd660e99561a470560717b474f69
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2017-02-22 09:06:37 +0100

    removed owner comment

--- src/backend/ParseData.java
+++ src/backend/ParseData.java
@@ -4,13 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * Created by emma on 2017-02-21.
- */
 public class ParseData {
 
     public static void main (String[] args) {
