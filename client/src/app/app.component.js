3cb2f0db00420ca2d9537d712c8c932d17019e89
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2017-02-27 15:09:58 +0100

    reseted from tutorial

--- client/src/app/app.component.js
+++ client/src/app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>Hello Angular</h1>'
+      template: '<h1>Hello Angular</h1>' //Url: 'hello.html'
     })
     .Class({
       constructor: function() {}
