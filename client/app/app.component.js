01110db9735f8ac166819b099d8bd9fb6f6d6a5d
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2017-03-02 14:52:12 +0100

    added folders, rearranged a bit

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>Hello Angular</h1>' //Url: 'hello.html'
+      templateUrl: './app/start.html'  //'<h1>Hello Angular</h1>' 
     })
     .Class({
       constructor: function() {}
